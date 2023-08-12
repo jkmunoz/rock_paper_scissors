0df1a9007ee3421db4fa2099f176123630995db4
jkmunoz <juliannakmunoz@gmail.com>
2023-08-11 22:27:33 -0500

    all combintations of user and comp. choices are functional

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,35 +1,25 @@
-var start = function () {
-    console.log('Hey there!' +
-    ' Thanks for playing Rock, Paper, Scissors!' +
-    ' You choose first, then the computer will choose too.');
-};
+function getUserChoice (userInput) {
+    userInput = prompt('Choose one... Rock, Paper, or Scissors?').toLowerCase();
 
-function getUserChoice (userChoice) {
-    console.log('Ready? Here we go!');
-    userChoice = prompt('Choose one... Rock, Paper, or Scissors?').toLowerCase();
-
-    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
-        return userChoice
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput
     } else {
         console.log('That is not a valid option.');
-        return getUserChoice(userChoice);
+        return getuserInput(userInput);
     }
 };
 
 function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random() * 2);
+    let randomNumber = Math.floor(Math.random() * 3);
 
-    switch (computerChoice) {
-        case 0:
-            console.log('rock');
-            break;
-        case 1:
-            console.log('paper');
-            break;
-        case 2:
-            console.log('scissors');
-            break;
-}};
+    if (randomNumber === 0) {
+        return 'rock';
+    } else if (randomNumber === 1) {
+        return 'paper';
+    } else if (randomNumber === 2) {
+        return 'scissors';
+    }
+};
 
 function youWon () {
     console.log('You won! wanna play again?');
@@ -39,37 +29,48 @@ function youWon () {
 function youLost () {
     console.log('Sorry, you lost this time. Wanna play again?');
     // add options to start again or say "goodbye".
-}
+};
 
 function determineWinner (userChoice, computerChoice) {
     //The computer wins.
     if (userChoice === computerChoice) {
-        console.log('Thats a tie, play again!');
-        return getUserChoice(); }
-    else if (userChoice === 'rock' && computerChoice =='paper') {
-            return youLost(); }
-    else if (userChoice === 'paper' && computerChoice == 'scissors') {
-            return youLost(); }
-    else if (userChoice === 'scissors' && computerChoice == 'rock') {
-        return youLost();
-    //If this function is called before userChoice or computerChoice.
-    } else if (userChoice === undefined){
-        console.log('Someone has not played yet!');
-        return getUserChoice();
-    } else if (computerChoice === undefined) {
-        console.log('The computer has not played...');
-        return getComputerChoice();
-    //There is a tie.
-    } else {
-        return youWon();
-    }};
+        console.log("It's a tie!");
+    } 
+    
+    //All outcomes when user chooses rock.
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return youLost();
+        }
 
+        if (computerChoice === 'scissors') {
+            return youWon();
+        }
+    }
+    
+    //All outcomes when user chooses paper.
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return youLost(); 
+        }
 
-// function playGame() {
-//     start();
-//     getUserChoice();
-//     getComputerChoice();
-//     determineWinner();
-// }
+        if (computerChoice === 'rock') {
+            return youWon();
+        }
+    }
 
+    //All ourcomes when user chooses scissors. 
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return youLost(); 
+        }
+        if (computerChoice === 'paper') {
+            return youWon();
+        }
+    }
+};
 
+    var userChoice = getUserChoice();
+    var computerChoice = getComputerChoice();
+
+    determineWinner(userChoice, computerChoice);
